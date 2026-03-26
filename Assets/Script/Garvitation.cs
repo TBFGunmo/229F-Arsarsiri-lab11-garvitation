94110c4ca400f22a7e865a4f129afa7cdbe9bddd
TBFGunmo <gunmohome123@gmail.com>
2026-03-26 10:31:33 +0700

    finish lab

--- Assets/Script/Garvitation.cs
+++ Assets/Script/Garvitation.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Garvitation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    Rigidbody rb;
+    public static List<Garvitation> otherObj;
+    const float G = 0.000667f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if (otherObj == null) 
+        {
+            otherObj = new List<Garvitation>();
+        }
+        otherObj.Add(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        foreach (Garvitation obj in otherObj) 
+        {
+            if (obj != this) 
+            {
+                Attract(obj);
+            }
+        }
     }
+
+    private void Attract(Garvitation other) 
+    {
+        Rigidbody otherRigidbody = other.rb;
+        Vector3 direction = rb.position - otherRigidbody.position;
+
+        float distance = direction.magnitude;
+        if (distance == 0f) return;
+
+        // F = G(m1 * m2) / r^2
+
+        float forceMagnitude = G * (rb.mass * otherRigidbody.mass) / Mathf.Pow(distance, 2);
+        Vector3 garvitationalForce = forceMagnitude * direction.normalized;
+        otherRigidbody.AddForce(garvitationalForce);
+
+
+    }
+
 }
